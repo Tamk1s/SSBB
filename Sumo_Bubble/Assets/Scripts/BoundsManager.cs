ba03fd64496fc4eb7446fc6119897393b8e1b627
MatthewWojtechko <matthew@lopsidegames.com>
2025-01-25 23:02:41 -0500

    Bounds Shrinks
    Simple shrinking logic for the bounds

---
+++ Sumo_Bubble/Assets/Scripts/BoundsManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundsManager : MonoBehaviour
+{
+    public GameObject North;
+    public GameObject South;
+    public GameObject East;
+    public GameObject West;
+
+    public float ShrinkSeconds = 90;
+
+
+    private Vector3 topStart, bottomStart, leftStart, rightStart;
+
+    private Vector3 finalPosition;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PLAYER")
+        {
+            Debug.Log("Game Over!");
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        topStart = North.transform.position;
+        bottomStart = South.transform.position;
+        leftStart = West.transform.position;
+        rightStart = East.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        North.transform.position = Vector3.Lerp(topStart, finalPosition, Time.time / ShrinkSeconds);
+        South.transform.position = Vector3.Lerp(bottomStart, finalPosition, Time.time / ShrinkSeconds);
+        East.transform.position = Vector3.Lerp(rightStart, finalPosition, Time.time / ShrinkSeconds);
+        West.transform.position = Vector3.Lerp(leftStart, finalPosition, Time.time / ShrinkSeconds);
+    }
+}
